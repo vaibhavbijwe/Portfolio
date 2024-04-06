91aaeb72ef9599e52b9137d4a511575cf342ad0d
Vaibhav Bijwe <vaibhavbijwe30@gmail.com>
2024-04-06 23:02:27 +0530

    second commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -13,14 +13,63 @@ const links = [
     { url: "/contact", title: "Contact"},
 ];
 
-const Navbar = () =>{
-  const [open, setOpen] = useState(false);
+function Navbar() {
+const [open,setOpen] = useState(false);
 
-  const topVariants={}
+const topVariants={
+  closed:{
+    rotate:0,
+  },
+  Opened:{
+    rotate:45,
+    backgroundColor:"rgb(255,255,255)",
+  },
 }
 
-function Navbar() {
-const [open,setOpen] = useState(false)
+const centerVariants={
+  closed:{
+    opacity:1,
+  },
+  Opened:{
+    opacity:0,
+  },
+}
+
+const bottomVariants={
+  closed:{
+    rotate:0,
+  },
+  Opened:{
+    rotate:-45,
+    backgroundColor:"rgb(255,255,255)"
+  },
+};
+
+const listVariants = {
+  closed:{
+    x:"100vh",
+  },
+  opened:{
+    x:0,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const ListItemVariants = {
+  closed: {
+    x: -10,
+    opacity: 0,
+  },
+  opened: {
+    x: 0,
+    opacity: 1,
+  },
+};
+
+
   return (
     <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
         <div className='hidden md:flex gap-4 w-1/3'>
@@ -32,7 +81,8 @@ const [open,setOpen] = useState(false)
       <div className='md:hidden lg:flex  xl:w-1/3 xl:justify-center'>
         <Link href="/" className='text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center'>
         <span className='text-white mr-1'>lama</span>
-        <span className= 'w-12 h-8 rounded bg-white flex items-center justify-center'>.dev</span>
+        <span className= 'w-12 h-8 rounded bg-white text-black flex items-center justify-center'>
+          .dev</span>
         </Link>
       </div>
       {/* Responsive */}
@@ -58,27 +108,43 @@ const [open,setOpen] = useState(false)
       <Image src="/x.png" alt='' width={24} height={24}/>
       </Link>
      </div>
-      <div className='md:hidden w-1/3'>
+      <div className='md:hidden'>
         {/* Menu button */}
         <button className='w-10 h-8 flex flex-col justify-between z-50 relative' 
-        onClick= {(()=>setOpen(!open))}>
-            <motion.div className='w-10 h-1 bg-black rounded'></motion.div>
-            <motion.div className='w-10 h-1 bg-black rounded'></motion.div>
-            <motion.div className='w-10 h-1 bg-black rounded'></motion.div>
+        onClick= {() => setOpen((prev) => !prev)}
+        >
+            <motion.div 
+            variants={topVariants} 
+            animate={open ? "opened" : "closed"} 
+            className='w-10 h-1 bg-black rounded origin-left'>
+
+            </motion.div>
+            <motion.div
+            variants={centerVariants} 
+            animate={open ? "opened" : "closed"} 
+            className='w-10 h-1 bg-black rounded'>
+
+            </motion.div>
+            <motion.div
+            variants={bottomVariants} 
+            animate={open ? "opened" : "closed"} 
+            className='w-10 h-1 bg-black rounded origin-left'>
+
+            </motion.div>
         </button>
         {/* Menu list  */}
-        {( open &&
-        <div className='absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl'>
-            {links.map(link =>(
-                <Link href={link.URL} key={link.title}>{link.title}</Link>
+        {open && (
+        <motion.div variants={listVariants} initial="closed" animate="opened" className='absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-40'>
+            {links.map((link) => (
+              <motion.div variants={ListItemVariants} className='' key={link.title}>
+                <Link href={link.url}>{link.title}</Link>
+                </motion.div>
             ))}
-
-        </div>
+        </motion.div>
         )}
       </div>
-        
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,11 +4,11 @@ const Homepage = () => {
   return (
   <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
     {/* image container */}
-    <div className="h-1/2 lg:h-full lg:w-1/2 relative ">
-      <Image src="/hero.png" alt="" fill className="object-contain" />
+    <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+     <Image src="/hero.png" alt="" fill className="object-contain" />
     </div>
     {/* text container */}
-    <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center ">
+    <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
       {/* tittle */}
       <h1 className="text-4xl md:text-6xl font-bold">Crafting Digital Experiences, Desigining Tomorrow.</h1>
       {/* DEscription  */}
